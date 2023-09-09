933797e54a84e0cdc12d3e8bc475ad09a37214ae
Omar Hady <ohady1992@gmail.com>
2023-09-09 14:10:05 -0700

    Bug fix trial

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
+    from calculator_1 import add, sub
 
     if a < b:
         c = add(a, b)
